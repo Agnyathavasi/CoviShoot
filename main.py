b7c1160fb4254e2763a035657bd2df1e0b44727d
Agnyathavasi <vishwajithhh12345@gmail.com>
2021-07-23 10:22:38 +0530

    Updated the bullet, jump and formatted the code

--- main.py
+++ main.py
@@ -56,10 +56,9 @@ start_img = pygame.image.load('img/start_btn.png').convert_alpha()
 exit_img = pygame.image.load('img/exit_btn.png').convert_alpha()
 restart_img = pygame.image.load('img/restart_btn.png').convert_alpha()
 # background
-pine1_img = pygame.image.load('img/Background/pine1.png').convert_alpha()
-pine2_img = pygame.image.load('img/Background/pine2.png').convert_alpha()
-mountain_img = pygame.image.load('img/Background/mountain.png').convert_alpha()
-sky_img = pygame.image.load('img/Background/sky_cloud.png').convert_alpha()
+bg_img_1 = pygame.image.load('img/Background/bg1.png').convert_alpha()
+bg_img_2 = pygame.image.load('img/Background/bg2.png').convert_alpha()
+bg_img_3 = pygame.image.load('img/Background/bg3.png').convert_alpha()
 # store tiles in a list
 img_list = []
 for x in range(TILE_TYPES):
@@ -99,12 +98,16 @@ def draw_text(text, font, text_col, x, y):
 
 def draw_bg():
     screen.fill(BG)
-    width = sky_img.get_width()
-    for x in range(5):
-        screen.blit(sky_img, ((x * width) - bg_scroll * 0.5, 0))
-        screen.blit(mountain_img, ((x * width) - bg_scroll * 0.6, SCREEN_HEIGHT - mountain_img.get_height() - 300))
-        screen.blit(pine1_img, ((x * width) - bg_scroll * 0.7, SCREEN_HEIGHT - pine1_img.get_height() - 150))
-        screen.blit(pine2_img, ((x * width) - bg_scroll * 0.8, SCREEN_HEIGHT - pine2_img.get_height()))
+    width = bg_img_1.get_width()
+    if level == 1:
+        for x in range(5):
+            screen.blit(bg_img_1, ((x * width) - bg_scroll * 0.5, 0))
+    if level == 2:
+        for x in range(5):
+            screen.blit(bg_img_2, ((x * width) - bg_scroll * 0.5, 0))
+    if level == 3:
+        for x in range(5):
+            screen.blit(bg_img_3, ((x * width) - bg_scroll * 0.5, 0))
 
 
 # function to reset level
@@ -197,8 +200,8 @@ class Soldier(pygame.sprite.Sprite):
             self.direction = 1
 
         # jump
-        if self.jump and self.in_air == False:
-            self.vel_y = -11
+        if self.jump and not self.in_air:
+            self.vel_y = -16
             self.jump = False
             self.in_air = True
 
@@ -766,15 +769,15 @@ while run:
             run = False
         # keyboard presses
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
+            if event.key == pygame.K_a:
                 moving_left = True
-            if event.key == pygame.K_RIGHT:
+            if event.key == pygame.K_d:
                 moving_right = True
             if event.key == pygame.K_SPACE:
                 shoot = True
-            if event.key == pygame.K_LSHIFT:
+            if event.key == pygame.K_q:
                 grenade = True
-            if event.key == pygame.K_UP and player.alive:
+            if event.key == pygame.K_w and player.alive:
                 player.jump = True
                 jump_fx.play()
             if event.key == pygame.K_ESCAPE:
@@ -782,13 +785,13 @@ while run:
 
         # keyboard button released
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
+            if event.key == pygame.K_a:
                 moving_left = False
-            if event.key == pygame.K_RIGHT:
+            if event.key == pygame.K_d:
                 moving_right = False
             if event.key == pygame.K_SPACE:
                 shoot = False
-            if event.key == pygame.K_LSHIFT:
+            if event.key == pygame.K_q:
                 grenade = False
                 grenade_thrown = False
 
